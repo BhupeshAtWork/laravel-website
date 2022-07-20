29889894f05cd2e9855d0530f27dbb7b02480cf4
Bhupesh Pandey <bhupeshatwork333@gmail.com>
2022-07-20 23:01:25 +0530

    ..

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,7 +12,14 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Login</a>
                         </li>
+                    @elseif (Route::is('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Register</a>
+                        </li>
                     @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('register') }}">Register</a>
                         </li>
